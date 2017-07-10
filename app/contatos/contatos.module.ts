b1a561e7a1f4ab0309b85f948192aeec4c27c0c8
Bianca <bianca.rocha1711@gmail.com>
2017-07-10 14:31:47 -0300

    commit inicial

---
+++ app/contatos/contatos.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContatosListaComponent } from './contatos-lista.component';
+import { ContatoDetalheComponent } from './contato-detalhe.component';
+import { ContatoRoutingModule } from './contato-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ContatoRoutingModule
+  ],
+  declarations: [
+    ContatoDetalheComponent,
+    ContatosListaComponent
+  ],
+  exports: [
+    ContatosListaComponent
+  ]
+})
+
+export class ContatosModule {}
